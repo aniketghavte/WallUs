140ad886485b6054ac7b10b572082f8eee557855
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-11-25 21:22:48 +0530

    pins Sucessful

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Circles}  from 'react-loader-spinner'
+
+const Spinner = ({message}) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+        <Circles
+            height="80"
+            width="80"
+            radius="9"
+            color="#00BFFF"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass          
+            className="m-5"
+        />
+
+        <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner
