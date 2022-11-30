14444b5035a5eac02adea48d8991231df8d56b9f
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2022-11-30 20:05:26 +0530

    User Profile finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Routes, Route, useNavigate} from 'react-router-dom'
 
 import Login from './components/Login';
@@ -7,6 +7,15 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
   return (
     <Routes>
       
